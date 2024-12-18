f2cd7a1f16ddcfa421fce61a2568150e8552928a
Mento Bento <mbentogyo@gmail.com>
2024-12-18 15:33:45 +0800

    bug: page not loading at certain situations

--- js/prompter.js
+++ js/prompter.js
@@ -12,7 +12,7 @@ function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
-    return results === "null" ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    return results === "null" ? '' : decodeURIComponent(results[1].replace(/\+/g, ' ')); 
 }
 
 window.onload = function () {
@@ -63,7 +63,7 @@ function setText(index) {
     if (scrollingNow) pauseScroll();
     scrollPosition = 0;
     prompterContent.style.top = "0px";
-    prompterContent.innerHTML = "<br>" + data[index];
+    prompterContent.innerHTML = "<br>" + (data? data[index] : 'No content. Close the window.'); 
 }
 
 //Initial theme
